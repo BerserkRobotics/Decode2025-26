535fc39d4dbbdaf45856fab642ac5877dba49cfa
Vanguard Robotics 1 <tatertotsevcik@gmail.com>
2023-04-11 13:35:49 -0500

    pushbot :(

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/basicDrive.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "basicDrive")
+public class basicDrive extends LinearOpMode {
+
+    private DcMotor FrontRight;
+    private DcMotor BackRight;
+    private DcMotor FrontLeft;
+    private DcMotor BackLeft;
+
+    private final ElapsedTime intakeTimer = new ElapsedTime();
+    //intakeState = IntakeState.RESTING;
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        double Launch_Angle;
+
+        FrontRight = hardwareMap.get(DcMotor.class, "Front Right");
+        BackRight = hardwareMap.get(DcMotor.class, "Back Right");
+        FrontLeft = hardwareMap.get(DcMotor.class, "Front Left");
+        BackLeft = hardwareMap.get(DcMotor.class, "Back Left");
+
+
+        // Put initialization blocks here.
+        waitForStart();
+        FrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        FrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        if (opModeIsActive()) {
+            FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+            // Put run blocks here.
+
+
+                FrontRight.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x) / 1);
+                BackRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) / 1);
+                FrontLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x) / 1);
+                BackLeft.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x) / 1);
+
+                while (gamepad1.right_bumper) {
+
+                    FrontRight.setPower(0.5);
+                    BackRight.setPower(0.5);
+                    FrontLeft.setPower(-0.5);
+                    BackLeft.setPower(-0.5);
+                }
+
+                while (gamepad1.left_bumper) {
+
+                    FrontRight.setPower(-0.5);
+                    BackRight.setPower(-0.5);
+                    FrontLeft.setPower(0.5);
+                    BackLeft.setPower(0.5);
+                }
+                telemetry.update();
+            }
+        }
+    }
+
+
+

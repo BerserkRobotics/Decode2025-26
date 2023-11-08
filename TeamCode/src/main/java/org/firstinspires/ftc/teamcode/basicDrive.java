20c825adf0a44cd803fb7c494c057dcf3e0e0603
Vanguard Robotics 1 <tatertotsevcik@gmail.com>
2023-11-08 15:14:38 -0600

    copied from blocks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicDrive.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "NewDrive (Blocks to Java)")
+@TeleOp(name = "NewDrive")
 class NewDrive extends LinearOpMode {
 
     private DcMotor FrontLeft;

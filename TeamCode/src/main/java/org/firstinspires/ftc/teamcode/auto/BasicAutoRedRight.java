0dbb90de1f1d20b7e29c995de0e414bac296e42f
Vanguard Robotics 1 <tatertotsevcik@gmail.com>
2023-11-14 12:07:34 -0600

    fixed names, actually this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BasicAutoRedRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BasicAutoRedRight.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "BasicAutoRedLeft")
+@Autonomous(name = "BasicAutoRedRight")
 public class BasicAutoRedRight extends LinearOpMode {
 
     private DcMotor ArmMotor;

2ecdfda86d8c6cf16f6522cf11cfd91d620fb97d
Vanguard Robotics 1 <tatertotsevcik@gmail.com>
2024-01-15 14:55:43 -0600

    teleop update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/basicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/basicDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "basicDrive (Blocks to Java)")
+@TeleOp(name = "basicDrive")
 public class basicDrive extends LinearOpMode {
 
     private DcMotor FrontRight;

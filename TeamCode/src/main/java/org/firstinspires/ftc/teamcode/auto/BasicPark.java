6c34efc83db17128dc262907c3ca8e2d488d8ac3
Vanguard Robotics 1 <tatertotsevcik@gmail.com>
2024-01-19 15:32:08 -0600

    teleop update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BasicPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BasicPark.java
@@ -39,8 +39,8 @@ public class BasicPark extends LinearOpMode {
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        BackLeft.setDirection(DcMotor.Direction.FORWARD);
-        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
         FrontLeft.setDirection(DcMotor.Direction.FORWARD);
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
         ;
@@ -55,8 +55,8 @@ public class BasicPark extends LinearOpMode {
             FrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             BackLeft.setTargetPosition(-1500);
             BackRight.setTargetPosition(-1500);
-            FrontLeft.setTargetPosition(1500);
-            FrontRight.setTargetPosition(1500);
+            FrontLeft.setTargetPosition(-1500);
+            FrontRight.setTargetPosition(-1500);
             BackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             BackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             FrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -65,7 +65,9 @@ public class BasicPark extends LinearOpMode {
             BackRight.setPower(0.4);
             FrontLeft.setPower(0.4);
             FrontRight.setPower(0.4);
-            sleep(1000);
+            sleep(3000);
+
+            //fwd
             BackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             BackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -82,6 +84,9 @@ public class BasicPark extends LinearOpMode {
             BackRight.setPower(0.4);
             FrontLeft.setPower(0.4);
             FrontRight.setPower(0.4);
+            sleep(1000);
+
+
 
             PlaneLift.setPosition(.15);
             sleep(1000);

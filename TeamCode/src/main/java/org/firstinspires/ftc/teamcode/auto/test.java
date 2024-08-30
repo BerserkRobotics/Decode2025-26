5e6780e97e150a6faec0404768365ad0078e734a
Emily VanCleave <evancleave8@gmail.com>
2024-08-30 14:27:34 -0500

    testing new update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/test.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+    @Autonomous(name = "test")
+    public class test extends LinearOpMode {
+
+        @Override
+        public void runOpMode() {
+
+            telemetry.addLine("H e l l o  W o r l d");
+
+
+        }
+    }

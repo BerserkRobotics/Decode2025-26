852eccbd4b46bc03f1063b9799f7bafa79dd2b44
Emily VanCleave <evancleave8@gmail.com>
2024-09-11 14:03:32 -0500

    two person drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TwoPersonDrive.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "testDrive")
+public class TwoPersonDrive extends LinearOpMode {
+
+    private DcMotor FrontRight;
+    private DcMotor BackRight;
+    private DcMotor FrontLeft;
+    private DcMotor BackLeft;
+
+    @Override
+    public void runOpMode() {
+        // Initialize motors
+        FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
+        BackRight = hardwareMap.get(DcMotor.class, "BackRight");
+        FrontLeft = hardwareMap.get(DcMotor.class, "FrontLeft");
+        BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
+
+        // Set motor directions
+        FrontRight.setDirection(DcMotor.Direction.FORWARD);
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        // Set zero power behavior
+        FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Initialize motor power variables
+            double front_left_power = 0;
+            double front_right_power = 0;
+            double back_left_power = 0;
+            double back_right_power = 0;
+
+            double turnSpeed1;
+            if (gamepad1.left_bumper) {
+                turnSpeed1 = -1;
+            } else if (gamepad1.right_bumper) {
+                turnSpeed1 = 1;
+            } else {
+                turnSpeed1 = 0;
+            }
+
+            double turnSpeed2;
+            if (gamepad2.left_bumper) {
+                turnSpeed2 = -1;
+            } else if (gamepad2.right_bumper) {
+                turnSpeed2 = 1;
+            } else {
+                turnSpeed2 = 0;
+            }
+            double moveSpeed = -gamepad1.left_stick_y;
+            double strafeSpeed = gamepad1.left_stick_x;
+            double speedSetter = 0.6;
+
+            front_left_power  = (-gamepad2.left_stick_y + turnSpeed2 + gamepad2.left_stick_x) * speedSetter;
+            front_right_power = (-gamepad1.left_stick_y - turnSpeed1 - gamepad1.left_stick_x) * speedSetter;
+            back_left_power   = (-gamepad2.left_stick_y + turnSpeed2 - gamepad2.left_stick_x) * speedSetter;
+            back_right_power  = (-gamepad1.left_stick_y - turnSpeed1 + gamepad1.left_stick_x) * speedSetter;
+
+
+            // Set motor powers
+            FrontRight.setPower(front_right_power);
+            FrontLeft.setPower(front_left_power);
+            BackRight.setPower(back_right_power);
+            BackLeft.setPower(back_left_power);
+
+            // Update telemetry data
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Front Left Power", front_left_power);
+            telemetry.addData("Front Right Power", front_right_power);
+            telemetry.addData("Back Left Power", back_left_power);
+            telemetry.addData("Back Right Power", back_right_power);
+            telemetry.update();
+        }
+    }
+}
